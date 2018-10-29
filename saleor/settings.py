799450a8f15fe48ea7cd70e4f1c2db1c6e631681
Brian Kelly <brian_kelly_ps@hotmail.com>
2018-10-29 16:28:02 -0400

    config settings

--- saleor/settings.py
+++ saleor/settings.py
@@ -162,7 +162,8 @@ TEMPLATES = [{
         'string_if_invalid': '<< MISSING VARIABLE "%s" >>' if DEBUG else ''}}]
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'f#!7o@hs=v5$qqb-_kgjgp97f7go3&sq^m5t5x^'
 
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
